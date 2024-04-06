3d76ab4c98c66fc39800151e4cf4f56e2891b880
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2024-04-07 03:17:48 +0530

    added click function

--- maptest.py
+++ maptest.py
@@ -21,3 +21,15 @@ map = folium.Map(location=location, zoom_start= 18)
 folium_static = map._repr_html_()
 st.components.v1.html(folium_static, width = 1080, height = 760)
 
+circle_radius = 10
+def on_mouse_event(event, x, y, flags, param):
+    global image  # Make sure to use the global image variable
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(image, (x, y), circle_radius, (0, 0, 255), 2)
+
+        st.write(f"Clicked at ({x}, {y})")
+cv2.setMouseCallback("Satellite Image", on_mouse_event)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
