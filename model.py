e77e2818472ec240fb799dcc50e1ac2eca686c4e
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2024-04-07 04:39:23 +0530

    u

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ co = cohere.Client('SvMpSTWkpUfpZkzWB4OAx7kWdsDARN4gVO24Qq6I') # This is your tr
 
 
 user_inputs = st.text_input('What type of a plant/tree are you looking for?')
-inputs = []
+inputs = [user_inputs]
 if st.button("Get recommendation"):
     response = co.classify(
     model='e7cd0c96-a994-46ab-be86-cd3fe11d5809-ft',
