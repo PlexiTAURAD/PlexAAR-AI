57bde129d7e5f255191275b1a8dbc790350192ee
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2024-04-07 03:23:37 +0530

    tried a solution

--- maptest.py
+++ maptest.py
@@ -4,31 +4,58 @@ import folium
 import os
 import streamlit as st
 import cv2
-
+import numpy as np
+from PIL import Image
+from io import BytesIO
 
 baseurl = 'https://nominatim.openstreetmap.org/search?format=json'
 address = "Aparna hill park, Hyderabad"
 response = requests.get(f"{baseurl}&q={address}")
 data = response.json()
-
-
 latitude = data[0].get('lat')
 longitude = data[0].get('lon')
-
 bbox = data[0].get('boundingbox')
 location = float(latitude), float(longitude)
-map = folium.Map(location=location, zoom_start= 18)
+
+map = folium.Map(location=location, zoom_start=18)
 folium_static = map._repr_html_()
-st.components.v1.html(folium_static, width = 1080, height = 760)
+st.components.v1.html(folium_static, width=1080, height=760)
+
+html = folium_static.split('<body>')[1].split('</body>')[0]
+html_encoded = base64.b64encode(html.encode()).decode()
+
+html_file = f"""
+<html>
+<body>
+<img src="data:text/html;base64,{html_encoded}">
+</body>
+</html>
+"""
+
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get(f"data:text/html;base64,{html_encoded}")
+driver.get_screenshot_as_file("map.png")
+driver.quit()
+
+image = cv2.imread("map.png")
+
+cv2.namedWindow("Satellite Image", cv2.WINDOW_NORMAL)
+cv2.imshow("Satellite Image", image)
 
 circle_radius = 10
+
 def on_mouse_event(event, x, y, flags, param):
-    global image  # Make sure to use the global image variable
+    global image  
 
     if event == cv2.EVENT_LBUTTONDOWN:
+       
         cv2.circle(image, (x, y), circle_radius, (0, 0, 255), 2)
 
+        cv2.imshow("Satellite Image", image)
+
         st.write(f"Clicked at ({x}, {y})")
+
 cv2.setMouseCallback("Satellite Image", on_mouse_event)
 
 cv2.waitKey(0)
