613f4188ddb4b3109fb0c871177afb9c511c969b
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2024-04-07 04:30:36 +0530

    model1

--- model.py
+++ model.py
@@ -1,9 +1,22 @@
 import cohere
-from cohere.classify import Example
+import streamlit as st
 
 co = cohere.Client('SvMpSTWkpUfpZkzWB4OAx7kWdsDARN4gVO24Qq6I') # This is your trial API key
-response = co.classify(
-  model='e7cd0c96-a994-46ab-be86-cd3fe11d5809-ft',
-  inputs=["I want a tree that looks beautiful","I want a tree that looks beautiful","I want a tree that looks beautiful","I want a tree that looks beautiful","I want a tree that looks beautiful","I want a tree that looks beautiful"])
 
-print('The confidence levels of the labels are: {}'.format(response.classifications))
+inputs = []
+inputs = st.text_input('What type of a plant/tree are you looking for?')
+if st.button("Get recommendation"):
+    response = co.classify(
+    model='e7cd0c96-a994-46ab-be86-cd3fe11d5809-ft',
+    inputs=inputs,
+    examples=[])
+    predictions =response.classifications[0].predictions
+    formatted_predictions = []
+
+
+formatted_predictions.append("The best trees for you are:")
+for i, prediction in enumerate(predictions, start=1):
+    formatted_predictions.append(f"{i}. {prediction}")
+numbered_list = "\n".join(formatted_predictions)
+st.write(numbered_list)
+
