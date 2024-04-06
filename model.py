10e607773ad742508e082bb4a3ed72d74833fab3
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2024-04-07 04:38:10 +0530

    fixed small error

--- model.py
+++ model.py
@@ -3,8 +3,9 @@ import streamlit as st
 
 co = cohere.Client('SvMpSTWkpUfpZkzWB4OAx7kWdsDARN4gVO24Qq6I') # This is your trial API key
 
+
+user_inputs = st.text_input('What type of a plant/tree are you looking for?')
 inputs = []
-inputs = st.text_input('What type of a plant/tree are you looking for?')
 if st.button("Get recommendation"):
     response = co.classify(
     model='e7cd0c96-a994-46ab-be86-cd3fe11d5809-ft',
