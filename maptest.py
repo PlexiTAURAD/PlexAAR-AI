2f0904163897e65376f4249e60c39180fa512591
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2024-04-07 05:36:05 +0530

    test

--- maptest.py
+++ maptest.py
@@ -4,15 +4,17 @@ import folium
 from streamlit_folium import st_folium
 import os
 import streamlit as st
+import time
 baseurl = 'https://nominatim.openstreetmap.org/search?format=json'
 
 address = st.text_input('Enter your address')
-if st.button("Get area map"):
-    response = requests.get(f"{baseurl}&q={address}")
-    data = response.json()
-    latitude = data[0].get('lat')
-    longitude = data[0].get('lon')
-    bbox = data[0].get('boundingbox')
-    location = float(latitude), float(longitude)
-    map = folium.Map(location=location, width = 800, height= 400)
-    st_folium(map)
+
+response = requests.get(f"{baseurl}&q={address}")
+data = response.json()
+latitude = data[0].get('lat')
+longitude = data[0].get('lon')
+bbox = data[0].get('boundingbox')
+location = float(latitude), float(longitude)
+map = folium.Map(location=location, width = 800, height= 400)
+st_folium(map)
+    
